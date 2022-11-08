a9eca979eb2812c07bbc75d611fb2581cf6f4bc8
donstany <donstany79@gmail.com>
2022-11-08 16:50:48 +0200

    added mappings in Car profile remove rules for autorization in views

---
+++ AutoServiceManager.Web/Areas/Catalog/Mappings/CarProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoServiceManager.Web.Areas.Catalog.Models;
+using AutoMapper;
+using AutoServiceManager.Application.Features.Cars.Queries.GetAllCached;
+using AutoServiceManager.Application.Features.Cars.Queries.GetById;
+using AutoServiceManager.Application.Features.Cars.Commands.Create;
+using AutoServiceManager.Application.Features.Cars.Commands.Update;
+
+namespace AutoServiceManager.Web.Areas.Catalog.Mappings
+{
+    public class CarProfile : Profile
+    {
+        public CarProfile()
+        {
+            CreateMap<GetAllCarsCachedResponse, CarViewModel>().ReverseMap();
+            CreateMap<GetCarByIdResponse, CarViewModel>().ReverseMap();
+            CreateMap<CreateCarCommand, CarViewModel>().ReverseMap();
+            CreateMap<UpdateCarCommand, CarViewModel>().ReverseMap();
+        }
+    }
+}

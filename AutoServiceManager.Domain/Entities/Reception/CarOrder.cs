d1210bc13fe176bd905a7291ac83e04c7f803cf4
donstany <donstany79@gmail.com>
2022-11-09 14:50:25 +0200

    rename core domain entity to CarOrder

--- AutoServiceManager.Domain/Entities/Reception/CarOrder.cs
+++ AutoServiceManager.Domain/Entities/Reception/CarOrder.cs
@@ -3,7 +3,7 @@ using AspNetCoreHero.Abstractions.Domain;
 
 namespace AutoServiceManager.Domain.Entities.Reception
 {
-    public class Application : AuditableEntity
+    public class CarOrder : AuditableEntity
     {
         public DateTime Date { get; set; }
         public string Description { get; set; }

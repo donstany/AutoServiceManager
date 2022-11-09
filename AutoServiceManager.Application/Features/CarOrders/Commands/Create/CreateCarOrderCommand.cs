068e7e23517e271edc93231f341d980c72d9ca68
donstany <donstany79@gmail.com>
2022-11-09 15:06:24 +0200

    added CreateCarOrderCommand

---
+++ AutoServiceManager.Application/Features/CarOrders/Commands/Create/CreateCarOrderCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoServiceManager.Application.Interfaces.Repositories;
+using AspNetCoreHero.Results;
+using AutoMapper;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using AutoServiceManager.Domain.Entities.Reception;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Commands.Create
+{
+    public partial class CreateCarOrderCommand : IRequest<Result<int>>
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int CarId { get; set; }
+    }
+
+    public class CreateCarOrderCommandHandler : IRequestHandler<CreateCarOrderCommand, Result<int>>
+    {
+        private readonly ICarOrderRepository _carOrderRepository;
+        private readonly IMapper _mapper;
+
+        private IUnitOfWork _unitOfWork { get; set; }
+
+        public CreateCarOrderCommandHandler(ICarOrderRepository carOrderRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _carOrderRepository = carOrderRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<int>> Handle(CreateCarOrderCommand request, CancellationToken cancellationToken)
+        {
+            var carOrder = _mapper.Map<CarOrder>(request);
+            await _carOrderRepository.InsertAsync(carOrder);
+            await _unitOfWork.Commit(cancellationToken);
+            return Result<int>.Success(carOrder.Id);
+        }
+    }
+}
---
+++ AutoServiceManager.Application/Interfaces/Repositories/ICarOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using AutoServiceManager.Domain.Entities.Reception;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoServiceManager.Application.Interfaces.Repositories
+{
+    public interface ICarOrderRepository
+    {
+        IQueryable<CarOrder> CarOrders { get; }
+
+        Task<List<CarOrder>> GetListAsync();
+
+        Task<CarOrder> GetByIdAsync(int carOrderId);
+
+        Task<int> InsertAsync(CarOrder carOrder);
+
+        Task UpdateAsync(CarOrder carOrder);
+
+        Task DeleteAsync(CarOrder carOrder);
+    }
+}

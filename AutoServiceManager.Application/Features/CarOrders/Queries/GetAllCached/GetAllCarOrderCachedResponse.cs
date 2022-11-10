6dca19417914c869a22f439b3485e6e2598d1825
donstany <donstany79@gmail.com>
2022-11-10 15:48:59 +0200

    added GetAllCarOrdersCachedQuery

---
+++ AutoServiceManager.Application/Features/CarOrders/Queries/GetAllCached/GetAllCarOrderCachedResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Queries.GetAllCached
+{
+    public class GetAllCarOrdersCachedResponse
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int CarId { get; set; }
+    }
+}
---
+++ AutoServiceManager.Application/Features/CarOrders/Queries/GetAllCached/GetAllCarOrdersCachedQuery.cs
@@ -0,0 +1,36 @@
+﻿using AutoServiceManager.Application.Interfaces.CacheRepositories;
+using AspNetCoreHero.Results;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Queries.GetAllCached
+{
+    public class GetAllCarOrdersCachedQuery : IRequest<Result<List<GetAllCarOrdersCachedResponse>>>
+    {
+        public GetAllCarOrdersCachedQuery()
+        {
+        }
+    }
+
+    public class GetAllCarOrdersCachedQueryHandler : IRequestHandler<GetAllCarOrdersCachedQuery, Result<List<GetAllCarOrdersCachedResponse>>>
+    {
+        private readonly ICarOrderCacheRepository _carOrderCacheRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllCarOrdersCachedQueryHandler(ICarOrderCacheRepository carOrderCacheRepository, IMapper mapper)
+        {
+            _carOrderCacheRepository = carOrderCacheRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<GetAllCarOrdersCachedResponse>>> Handle(GetAllCarOrdersCachedQuery request, CancellationToken cancellationToken)
+        {
+            var carOrderList = await _carOrderCacheRepository.GetCachedListAsync();
+            var mappedCarOrders = _mapper.Map<List<GetAllCarOrdersCachedResponse>>(carOrderList);
+            return Result<List<GetAllCarOrdersCachedResponse>>.Success(mappedCarOrders);
+        }
+    }
+}

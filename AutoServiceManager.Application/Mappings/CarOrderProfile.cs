f8534202acba4cf34e697ca60f2f6da7c0f93254
donstany <donstany79@gmail.com>
2022-11-11 11:30:23 +0200

    added CarOrderProfile in Application assembly

---
+++ AutoServiceManager.Application/Mappings/CarOrderProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoServiceManager.Domain.Entities.Catalog;
+using AutoMapper;
+using AutoServiceManager.Application.Features.CarOrders.Commands.Create;
+using AutoServiceManager.Application.Features.CarOrders.Queries.GetById;
+using AutoServiceManager.Application.Features.CarOrders.Queries.GetAllCached;
+using AutoServiceManager.Application.Features.CarOrders.Queries.GetAllPaged;
+
+namespace AutoServiceManager.Application.Mappings
+{
+    internal class CarOrderProfile : Profile
+    {
+        public CarOrderProfile()
+        {
+            CreateMap<CreateCarOrderCommand, Product>().ReverseMap();
+            CreateMap<GetCarOrderByIdResponse, Product>().ReverseMap();
+            CreateMap<GetAllCarOrdersCachedResponse, Product>().ReverseMap();
+            CreateMap<GetAllCarOrdersResponse, Product>().ReverseMap();
+        }
+    }
+}

54fa18e14b41a710692cc6f697f53e4cc21e5142
donstany <donstany79@gmail.com>
2022-11-11 11:43:47 +0200

    added CarOrderViewModelValidator

---
+++ AutoServiceManager.Web/Areas/Reception/Validators/CarOrderViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using AutoServiceManager.Web.Areas.Reception.Models;
+using FluentValidation;
+
+namespace AutoServiceManager.Web.Areas.Reception.Validators
+{
+    public class CarOrderViewModelValidator : AbstractValidator<CarOrderViewModel>
+    {
+        public CarOrderViewModelValidator()
+        {
+            RuleFor(p => p.Description)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MinimumLength(5).WithMessage("{PropertyName} must greather than 5 characters.")
+                .MaximumLength(300).WithMessage("{PropertyName} must not exceed 300 characters.");
+
+            RuleFor(p => p.CarId)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull();
+        }
+    }
+}

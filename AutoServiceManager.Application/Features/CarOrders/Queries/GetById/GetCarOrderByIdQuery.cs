869ca23007fa0bec4ee20693b72bb84a14e8df6a
donstany <donstany79@gmail.com>
2022-11-11 10:37:50 +0200

    added GetCarOrderByIdQuery feature in Queries

---
+++ AutoServiceManager.Application/Features/CarOrders/Queries/GetById/GetCarOrderByIdQuery.cs
@@ -0,0 +1,33 @@
+﻿using AutoServiceManager.Application.Interfaces.CacheRepositories;
+using AspNetCoreHero.Results;
+using AutoMapper;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Queries.GetById
+{
+    public class GetCarOrderByIdQuery : IRequest<Result<GetCarOrderByIdResponse>>
+    {
+        public int Id { get; set; }
+
+        public class GetCarOrderByByIdQueryHandler : IRequestHandler<GetCarOrderByIdQuery, Result<GetCarOrderByIdResponse>>
+        {
+            private readonly ICarOrderCacheRepository _carOrderCache;
+            private readonly IMapper _mapper;
+
+            public GetCarOrderByByIdQueryHandler(ICarOrderCacheRepository carOrderCache, IMapper mapper)
+            {
+                _carOrderCache = carOrderCache;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<GetCarOrderByIdResponse>> Handle(GetCarOrderByIdQuery query, CancellationToken cancellationToken)
+            {
+                var carOrder = await _carOrderCache.GetByIdAsync(query.Id);
+                var mappedCarOrder = _mapper.Map<GetCarOrderByIdResponse>(carOrder);
+                return Result<GetCarOrderByIdResponse>.Success(mappedCarOrder);
+            }
+        }
+    }
+}
---
+++ AutoServiceManager.Application/Features/CarOrders/Queries/GetById/GetCarOrderByIdResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Queries.GetById
+{
+    public class GetCarOrderByIdResponse
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int CarId { get; set; }
+    }
+}

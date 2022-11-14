3ca716dab1c811aa01e242617c92786289e0ad97
donstany <donstany79@gmail.com>
2022-11-14 13:45:56 +0200

    add description constraint

--- AutoServiceManager.Infrastructure/Migrations/ApplicationDb/20221114114515_add_column_constraint_description.Designer.cs
+++ AutoServiceManager.Infrastructure/Migrations/ApplicationDb/20221114114515_add_column_constraint_description.Designer.cs
@@ -4,14 +4,16 @@ using AutoServiceManager.Infrastructure.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutoServiceManager.Infrastructure.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114114515_add_column_constraint_description")]
+    partial class add_column_constraint_description
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AutoServiceManager.Infrastructure/Migrations/ApplicationDb/20221114114515_add_column_constraint_description.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoServiceManager.Infrastructure.Migrations.ApplicationDb
+{
+    public partial class add_column_constraint_description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "CarOrders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "CarOrders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}

1cf943138377f5b81d74e5be0375c1cfc6a88a98
donstany <donstany79@gmail.com>
2022-11-14 16:03:47 +0200

    added tab in sidebar added CarOrdersReportView

---
+++ AutoServiceManager.Web/Areas/Reception/Controllers/CarOrdersReportViewController.cs
@@ -0,0 +1,16 @@
+﻿using AutoServiceManager.Domain.Entities.Reception;
+using AutoServiceManager.Web.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoServiceManager.Web.Areas.Reception.Controllers
+{
+    [Area("Reception")]
+    public class CarOrdersReportViewController : BaseController<CarOrdersReportViewController>
+    {
+        public IActionResult Index()
+        {
+            var model = new CarOrdersReportView();
+            return View(model);
+        }
+    }
+}

83874a530a1f61084ba0bd969ccb13ec03a3c71c
donstany <donstany79@gmail.com>
2022-11-14 15:56:37 +0200

    added GetAllCarOrdersReportViewCachedQuery

---
+++ AutoServiceManager.Application/Features/CarOrdersView/Queries/GetAllCached/GetAllCarOrdersCachedQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoServiceManager.Application.Interfaces.CacheRepositories;
+using AspNetCoreHero.Results;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoServiceManager.Application.Features.CarOrdersView.Queries.GetAllCached;
+
+namespace AutoServiceManager.Application.Features.CarOrders.Queries.GetAllCached
+{
+    public class GetAllCarOrdersReportViewCachedQuery : IRequest<Result<List<GetAllCarOrdersReportViewCachedResponse>>>
+    {
+        public GetAllCarOrdersReportViewCachedQuery()
+        {
+        }
+    }
+
+    public class GetAllCarOrdersReportViewCachedQueryHandler : IRequestHandler<GetAllCarOrdersReportViewCachedQuery, Result<List<GetAllCarOrdersReportViewCachedResponse>>>
+    {
+        private readonly ICarOrdersReportViewCacheRepository _carOrdersReportViewCacheRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllCarOrdersReportViewCachedQueryHandler(ICarOrdersReportViewCacheRepository carOrdersReportViewCacheRepository, IMapper mapper)
+        {
+            _carOrdersReportViewCacheRepository = carOrdersReportViewCacheRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<GetAllCarOrdersReportViewCachedResponse>>> Handle(GetAllCarOrdersReportViewCachedQuery request, CancellationToken cancellationToken)
+        {
+            var carOrdersReportViewList = await _carOrdersReportViewCacheRepository.GetCachedListAsync();
+            var mappedCarOrdersReportViews = _mapper.Map<List<GetAllCarOrdersReportViewCachedResponse>>(carOrdersReportViewList);
+            return Result<List<GetAllCarOrdersReportViewCachedResponse>>.Success(mappedCarOrdersReportViews);
+        }
+    }
+}
---
+++ AutoServiceManager.Application/Features/CarOrdersView/Queries/GetAllCached/GetAllCarOrdersReportViewResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutoServiceManager.Application.Features.CarOrdersView.Queries.GetAllCached
+{
+    public class GetAllCarOrdersReportViewCachedResponse
+    {
+        public int CarOrderId { get; set; }
+        public DateTime CarOrderDate { get; set; }
+        public string CarData { get; set; }
+        public string CarOrderDescription { get; set; }
+        public string UserFullName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}

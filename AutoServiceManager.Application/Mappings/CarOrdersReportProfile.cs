c69cf58bd8d218aff8cb1230930f2af46887accf
donstany <donstany79@gmail.com>
2022-11-15 10:39:27 +0200

    added mapping in Application and in Web assembly

---
+++ AutoServiceManager.Application/Mappings/CarOrdersReportProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AutoServiceManager.Domain.Entities.Reception;
+using AutoServiceManager.Application.Features.CarOrdersView.Queries.GetAllCached;
+
+namespace AutoServiceManager.Application.Mappings
+{
+    internal class CarOrdersReportViewProfile : Profile
+    {
+        public CarOrdersReportViewProfile()
+        {
+            CreateMap<GetAllCarOrdersReportViewCachedResponse, CarOrder>().ReverseMap();
+        }
+    }
+}
---
+++ AutoServiceManager.Web/Areas/Reception/Mappings/CarOrdersReportProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoServiceManager.Web.Areas.Reception.Models;
+using AutoMapper;
+using AutoServiceManager.Application.Features.CarOrdersView.Queries.GetAllCached;
+
+namespace AutoServiceManager.Web.Areas.Reception.Mappings
+{
+    internal class CarOrdersReportViewProfile : Profile
+    {
+        public CarOrdersReportViewProfile()
+        {
+            CreateMap<GetAllCarOrdersReportViewCachedResponse, CarOrderViewModel>().ReverseMap();
+        }
+    }
+}
